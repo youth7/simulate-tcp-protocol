bbaffa6f10cc6a62322ea0ef4516ab4d4faa1574
chris huang <youth7@163.com>
2021-12-21 21:12:15 +0800

    走通发送的流程

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
-use std::{sync::{mpsc, Arc, Mutex}, thread, collections::LinkedList};
-#[derive(Debug)]
-struct Packet {}
 mod tcp_simulator;
+use std::{thread, time::Duration};
 
-type ShareBuffer = Arc<Mutex<LinkedList<Packet>>>;
-fn share_buffer() -> ShareBuffer {
-    let buffer: Arc<Mutex<LinkedList<Packet>>> = Arc::new(Mutex::new(LinkedList::new()));
-    let buffer1: Arc<Mutex<LinkedList<Packet>>> = Arc::clone(&buffer);
-    thread::spawn( move||{
-        // Arc::clone(&buffer).lock().unwrap()
-        buffer1.lock().unwrap();
-    });
-    buffer
-
-}
-
-
+use tcp_simulator::TCPSimulator;
 
 fn main() {
-    let buffer = share_buffer();
-    
-
+    let simulator = TCPSimulator::new();
+    simulator.three_way_hand_shake();
+    thread::sleep(Duration::from_secs(10000));
 }
-
-
-    // let (a, b) = channel::<Test>();
-
-    // thread::spawn(move ||  {
-    //     let data = b.recv();
-    // });
-
-    // use std::sync::mpsc::channel;
-    // use std::thread;
-    // use std::time::Duration;
-
-    // let (send, recv) = channel::<Test>();
-
-    // thread::spawn(move || {
-    //     println!("222 {:?}", recv.recv().unwrap()); // Received immediately
-    //     println!("Waiting...");
-    //     println!("222 {:?}", recv.recv().unwrap()); // Received after 2 seconds
-    // });
-
-    // send.send(Test {}).unwrap();
-    // thread::sleep(Duration::from_secs(2)); // block for two seconds
-    // send.send(Test {}).unwrap();
--- src/tcp_simulator.rs
+++ src/tcp_simulator.rs
@@ -1,14 +1,13 @@
 use std::collections::LinkedList;
-use std::sync::mpsc::{self, channel};
-use std::thread::{self, JoinHandle};
+use std::sync::mpsc;
+use std::thread;
 use std::{
-    rc::Rc,
     sync::{
         mpsc::{Receiver, Sender},
         Arc, Mutex,
     },
 };
-
+#[derive(Debug)]
 struct Packet {}
 struct EndPoint {
     port: u32,
@@ -36,18 +35,6 @@ impl Channel {
     }
 }
 
-// struct ShareBuffer{
-//     buffer : Arc<Mutex<LinkedList<Packet>>>,
-// }
-
-// impl ShareBuffer{
-//     fn new() -> ShareBuffer{
-//         ShareBuffer{
-//             buffer : Arc::new(Mutex::new(LinkedList::new())),
-//         }
-//     }
-// }
-
 type ShareBuffer = Arc<Mutex<LinkedList<Packet>>>;
 fn share_buffer() -> ShareBuffer {
     Arc::new(Mutex::new(LinkedList::new()))
@@ -67,22 +54,36 @@ impl Proxy {
     ) -> Proxy {
         let buffer1 = share_buffer();
         let buffer2 = share_buffer();
-        let proxy = Proxy {
-            buffer2 : Arc::clone(&buffer2),
-            buffer1: Arc::clone(&buffer1),
-        };
-
-        thread::spawn(move || loop {
-            let data = rx1.recv();
-        });
-        thread::spawn(|| loop {
-            let data = Arc::clone(&buffer1).lock().unwrap().pop_back();
-        });
-
-        proxy
+        Proxy::receive_data(Arc::clone(&buffer1), rx1);
+        Proxy::receive_data(Arc::clone(&buffer2), rx2);
+        Proxy::send_data(Arc::clone(&buffer1), tx1);
+        Proxy::send_data(Arc::clone(&buffer2), tx2);
+        Proxy {
+            buffer2,
+            buffer1,
+        }
     }
 
-    fn start(&self) {}
+    fn receive_data(buffer: ShareBuffer, rx: Receiver<Packet>) {
+        thread::spawn(move || loop {
+            let data = rx.recv().unwrap();
+            println!("从channel中读取到一个包{:?}", data);
+            let mut internal_buffer = buffer.lock().unwrap();
+            // println!("2{:?}", "2");
+            internal_buffer.push_back(data);
+            // println!("3{:?}", "3");
+        });
+    }
+
+    fn send_data(buffer: ShareBuffer, tx: Sender<Packet>) {
+        thread::spawn(move || loop {
+            let mut internal_buffer = buffer.lock().unwrap();
+            if let Some(data) = internal_buffer.pop_back() {
+                println!("从buffer中读取到一个包{:?}", data);
+                tx.send(data).unwrap();
+            }
+        });
+    }
 }
 /////////////////////////////////////////////////////////////////////
 pub struct TCPSimulator {
@@ -92,14 +93,14 @@ pub struct TCPSimulator {
 }
 // 模拟器里面负责触发各种场景，但是不负责具体的实现
 impl TCPSimulator {
-    fn new() -> TCPSimulator {
-        let (tx_end_porint1, rx1_proxy) = mpsc::channel::<Packet>(); // end point1 ⟾ proxy ⟾ end point2
-        let (tx1_proxy, rx_end_point2) = mpsc::channel::<Packet>();
-        let (tx_end_porint2, rx2_proxy) = mpsc::channel::<Packet>(); // end point2 ⟽ proxy ⟽ end point2
-        let (tx2_proxy, rx_end_point1) = mpsc::channel::<Packet>();
-        let end_point1 = EndPoint::new(123, tx_end_porint1, rx_end_point1);
-        let end_point2 = EndPoint::new(123, tx_end_porint2, rx_end_point2);
-        let proxy = Proxy::new(tx1_proxy, rx1_proxy, tx2_proxy, rx2_proxy);
+    pub fn new() -> TCPSimulator {
+        let (tx_in_end_porint1, rx1_in_proxy) = mpsc::channel::<Packet>(); // end point1 ⟾ proxy ⟾ end point2
+        let (tx1_in_proxy, rx_in_end_point2) = mpsc::channel::<Packet>();
+        let (tx_in_end_porint2, rx2_in_proxy) = mpsc::channel::<Packet>(); // end point2 ⟽ proxy ⟽ end point2
+        let (tx2_in_proxy, rx_in_end_point1) = mpsc::channel::<Packet>();
+        let end_point1 = EndPoint::new(123, tx_in_end_porint1, rx_in_end_point1);
+        let end_point2 = EndPoint::new(123, tx_in_end_porint2, rx_in_end_point2);
+        let proxy = Proxy::new(tx1_in_proxy, rx1_in_proxy, tx2_in_proxy, rx2_in_proxy);
         TCPSimulator {
             end_point1,
             end_point2,
@@ -108,7 +109,7 @@ impl TCPSimulator {
     }
 
     // 模拟三次握手的连接
-    fn three_way_hand_shake(&self) {
-        let sync = &self.end_point1.syn();
+    pub fn three_way_hand_shake(&self) {
+        &self.end_point1.syn();
     }
 }
