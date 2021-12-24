7f66ba9ea6b4353a1c3f1032a77c276243ba0475
chris huang <youth7@163.com>
2021-12-24 18:38:14 +0800

    #1 enpoint支持状态

--- src/component/end_point.rs
+++ src/component/end_point.rs
@@ -1,27 +1,60 @@
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
-use crate::data_structure::ipv4_packet::IPV4Packet;
+use crate::data_structure::ipv4_packet::{Flag, IPV4Header, IPV4Packet};
 
 pub struct EndPoint {
     port: u32,
     tx: Sender<IPV4Packet>,
+    // rx: Receiver<IPV4Packet>,
+    status: Status,
 }
 
 impl EndPoint {
     pub fn new(port: u32, tx: Sender<IPV4Packet>, rx: Receiver<IPV4Packet>) -> EndPoint {
-        EndPoint::on_receive_data(tx.clone(), rx);
-        EndPoint { port, tx }
+        // EndPoint::on_receive_data(tx.clone(), rx);
+        let end_point = EndPoint {
+            port,
+            tx:tx.clone(),
+            // rx,
+            status: Status::CLOSED,
+        };
+        end_point.start(rx, tx);
+        end_point
     }
     pub fn syn(&self) {
-        let syn_packet = IPV4Packet {};
+        let header = IPV4Header::new(6666, 7777, self.isn(), 0, Flag::SYN);
+        let syn_packet = IPV4Packet { header };
+
         self.tx.send(syn_packet).unwrap();
     }
-    fn on_receive_data(tx: Sender<IPV4Packet>, rx: Receiver<IPV4Packet>) {
+
+    pub fn start(&self, rx:Receiver<IPV4Packet>, tx:Sender<IPV4Packet>) {
         thread::spawn(move || loop {
             let data = rx.recv().unwrap();
             println!("endPoint：从channel中读取到一个包{:?}", data);
             tx.send(data).unwrap();
+            // self.handle();
         });
     }
+
+    fn handle(&self) {}
+
+    fn isn(&self) -> u32 {
+        return 0;
+    }
+}
+
+enum Status {
+    CLOSED,
+    LISTEN,
+    SYN_SENT,
+    SYN_RECEIVED,
+    ESTABLISHED,
+    FIN_WAIT_1,
+    FIN_WAIT_2,
+    CLOSE_WAIT,
+    CLOSING,
+    LAST_ACK,
+    TIME_WAIT,
 }
--- src/data_structure/ipv4_packet.rs
+++ src/data_structure/ipv4_packet.rs
@@ -1,2 +1,53 @@
+
 #[derive(Debug)]
-pub struct IPV4Packet {}
+pub struct IPV4Packet {
+    // payload: Box<[u8]>,
+    pub header: IPV4Header,
+}
+
+impl IPV4Packet {
+    pub fn new(header: IPV4Header) -> IPV4Packet {
+        IPV4Packet { header }
+    }
+}
+
+#[derive(Debug)]
+pub struct IPV4Header {
+    src_port: u16,
+    dest_port: u16,
+    seq: u32,
+    ack: u32,
+    // header_size: u8,
+    flag: Flag,
+    // window_size: u16,
+}
+
+impl IPV4Header {
+    pub fn new(src_port: u16, dest_port: u16, seq: u32, ack: u32, flag: Flag) -> IPV4Header {
+        IPV4Header {
+            src_port,
+            dest_port,
+            seq,
+            ack,
+            flag,
+        }
+    }
+
+    pub fn get_buffer() -> Box<[u8]> {
+        //将packet转为符合tcp规定的buffer
+        todo!();
+    }
+}
+
+#[derive(Debug)]
+pub enum Flag {
+    NS,
+    CWR,
+    ECE,
+    URG,
+    ACK,
+    PSH,
+    RST,
+    SYN,
+    FIN,
+}
