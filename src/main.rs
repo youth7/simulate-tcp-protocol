2222f4a93695120e18689e9a9f60de3e692a98fc
chris huang <youth7@163.com>
2021-12-31 19:04:29 +0800

    #1 临时提交

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
-mod tcp_simulator;
-mod data_structure;
 mod component;
+mod data_structure;
+mod tcp_simulator;
 use std::{thread, time::Duration};
 
 use tcp_simulator::TCPSimulator;
 
+struct Test {
+    name: u32,
+}
+
+impl Test {
+    pub fn new() -> Test{
+        let t = Test { name: 123 };
+        t.test();
+        t
+    }
+    pub fn test(&'static self){
+        thread::spawn(move ||{
+            self.do_sth();
+        });
+    }
+
+    pub fn do_sth(&self){
+        println!("{}", self.name);
+    }
+}
+
+
+
 fn main() {
-    let simulator = TCPSimulator::new();
-    simulator.three_way_hand_shake();
-    thread::sleep(Duration::from_secs(10000));
+    let t = Test::new();
+    t.do_sth();
+    // let simulator = TCPSimulator::new();
+    // simulator.three_way_hand_shake();
+    // thread::sleep(Duration::from_secs(10000));
 }
